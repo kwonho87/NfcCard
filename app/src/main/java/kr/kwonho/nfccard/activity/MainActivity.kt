86660329ee1615fe04eb0cb5c3d7ba74639febce
kwonho87 <kwonho87@gmail.com>
2020-07-24 14:44:14 +0900

    init project.

--- app/src/main/java/kr/kwonho/nfccard/activity/MainActivity.kt
+++ app/src/main/java/kr/kwonho/nfccard/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package kr.kwonho.nfccard
+package kr.kwonho.nfccard.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.kwonho.nfccard.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/kr/kwonho/nfccard/service/NfcService.kt
@@ -0,0 +1,2 @@
+package kr.kwonho.nfccard.service
+
